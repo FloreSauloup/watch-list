8f859d82fda970e6398560331ff2407fafa303af
Flore Sauloup <flore.sauloup@audencia.com>
2021-08-12 16:02:10 +0200

    exo deux

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
+end
